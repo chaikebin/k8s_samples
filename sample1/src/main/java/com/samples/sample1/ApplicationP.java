beae5e0bd62dc5760e55689227af5e44555a8654
柴克斌 <chaikebin@corp.netease.com>
2020-03-28 22:36:55 +0800

    update

--- sample1/src/main/java/com/samples/sample1/ApplicationP.java
+++ sample1/src/main/java/com/samples/sample1/ApplicationP.java
@@ -1,20 +1,15 @@
 package com.samples.sample1;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@ImportResource("classpath*:/META-INF/spring/app-*.xml")
 @RestController
-@RequestMapping("/api")
 public class ApplicationP {
 
   private List<byte[]> oom = new ArrayList<>();
@@ -23,26 +18,12 @@ public class ApplicationP {
     SpringApplication.run(ApplicationP.class, args);
   }
 
-  @RequestMapping(value = "/testString/string/{string}", method = RequestMethod.GET)
-  public String testString(@PathVariable("string") String string) {
-    return "ApplicationP:"+string;
-  }
-
   @RequestMapping(value="/oom")
-  public String oom(){
-    Thread t = new Thread(new Runnable() {
-      @Override
-      public void run() {
-        oom.add(new byte[1024*1024*1024]);
-        try {
-          Thread.sleep(10);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-      }
-    });
-    t.setName("oom");
-    t.start();
-    return "done";
+  public void oom(){
+    List<byte[]> l = new LinkedList<>();
+    while (true) {
+      l.add(new byte[1024*1024]);
+      System.out.println("free memory="+Runtime.getRuntime().freeMemory());
+    }
   }
 }
